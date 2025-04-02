0d8082d34ffa260cd21a105c04a6aaf02764ceea
Daniel Amado <daniel.amadove@gmail.com>
2025-04-02 18:25:52 -0500

    Refactor MenstrualPhase component layout; add informational buttons and update footer styling

--- src/components/MenstrualPhase.tsx
+++ src/components/MenstrualPhase.tsx
@@ -20,20 +20,38 @@ const MenstrualPhase = ({ dia }: Props) => {
       style={{ backgroundColor: colorBg }}
     >
       <div className="flex-1 flex">
-        <div className="max-w-7xl mx-auto w-full p-4 md:p-6 lg:p-8">
-          <div className="flex flex-col gap-4 lg:gap-8 h-full justify-between">
+        <div className="max-w-7xl mx-auto w-full p-4 md:p-6 lg:p-8 pb-0 md:pb-0 lg:pb-0">
+          <div className="flex flex-col gap-4 lg:gap-8 h-full justify-between relative">
             <Header color={colorHeaderAndFooter} />
             <div className="flex flex-col lg:flex-row gap-8 items-center justify-center">
-              <div>
+              <div className="flex-1">
                 <img
                   src={urlImg}
                   alt={`Imagen de ${faseName}`}
-                  className="h-96"
+                  
                 />
               </div>
-              <div></div>
+              <div className="min-w-80">
+                <ul>
+                  <li>
+                    <button>
+                      INFORMACION
+                    </button>
+                  </li>
+                  <li>
+                    <button>
+                      SINTOMAS
+                    </button>
+                  </li>
+                  <li>
+                    <button>
+                      ALIMENTACION
+                    </button>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <Footer color={colorHeaderAndFooter} />
+            <footer className="text-sm absolute bottom-0 py-4 md:py-6 lg:py-8" style={{color: colorHeaderAndFooter}}>PARA USO EXCLUSIVO DEL PROFESIONAL MÉDICO</footer>
           </div>
         </div>
       </div>
